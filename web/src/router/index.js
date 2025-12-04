686826f0931263cb7d6f5e842c644d28741d3479
qwas <git@qwas.fun>
2025-12-04 22:02:26 +0800

    文件上传

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import IndexView from '@/views/index/IndexView.vue'
+import HomeView from '@/views/index/HomeView.vue'
 
 import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
@@ -12,6 +13,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'index', component: IndexView },
+    { path: '/home', name: 'home', component: HomeView },
     { path: '/login', redirect: '/auth/login' },
     { path: '/auth/register', name: 'register', component: RegisterView },
     { path: '/auth/login', name: 'login', component: LoginView },
--- web/vite.config.js
+++ web/vite.config.js
@@ -4,10 +4,23 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), vue(), vueDevTools()],
+  plugins: [
+    tailwindcss(),
+    vue(),
+    vueDevTools(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
