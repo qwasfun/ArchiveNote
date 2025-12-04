f9c1d77ca3771ad0d8d43b56bb1222143d1fd502
qwas <git@qwas.fun>
2025-12-04 11:17:39 +0800

    feat(web): 实现的页面路由，注册登录界面

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,8 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import IndexView from '@/views/index/IndexView.vue'
+
+import LoginView from '@/views/auth/LoginView.vue'
+import RegisterView from '@/views/auth/RegisterView.vue'
+
+import NotesList from '@/views/notes/NotesList.vue'
+import NotesDetail from '@/views/notes/NotesDetail.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: '/', name: 'index', component: IndexView },
+    { path: '/login', redirect: '/auth/login' },
+    { path: '/auth/register', name: 'register', component: RegisterView },
+    { path: '/auth/login', name: 'login', component: LoginView },
+    { path: '/notes', name: 'notesList', component: NotesList },
+    { path: '/notes/:id', name: 'notesDetail', component: NotesDetail },
+  ],
 })
 
 export default router
---
+++ web/src/views/auth/LoginView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="pt-20">
+    <fieldset class="fieldset mx-auto bg-base-200 border-base-300 rounded-box w-xs border p-4">
+      <legend class="fieldset-legend">Login</legend>
+
+      <label class="label">username</label>
+      <input type="username" class="input" placeholder="username" />
+
+      <label class="label">Password</label>
+      <input type="password" class="input" placeholder="Password" />
+
+      <button class="btn btn-neutral mt-4">Login</button>
+      <div class="text-center pt-2">
+        <RouterLink class="link link-primary" to="/auth/register">Register</RouterLink>
+      </div>
+    </fieldset>
+  </div>
+</template>
